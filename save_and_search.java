c427212fdeab393cb454c40ff56b85fcd2c714db
Atom Scott <Atom Scott>
2016-09-23 17:10:15 +0900

    add example save

--- save_and_search.java
+++ save_and_search.java
@@ -1,10 +1,31 @@
 
 
-public class save_and_seach{
+public class save_and_search{
 
-  public static void main(string[]args){
-    System.out.print("hello")
-  }
+  public static void main(String[] args) {
+    URL url;
+    InputStream is = null;
+    BufferedReader br;
+    String line;
 
+    try {
+        url = new URL("http://www.jfa.jp/match/nationalsportsfestival_2016/women/schedule_result/");
+        is = url.openStream();  // throws an IOException
+        br = new BufferedReader(new InputStreamReader(is));
 
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+    } catch (MalformedURLException mue) {
+         mue.printStackTrace();
+    } catch (IOException ioe) {
+         ioe.printStackTrace();
+    } finally {
+        try {
+            if (is != null) is.close();
+        } catch (IOException ioe) {
+            // nothing to see here
+        }
+    }
+}
 }
